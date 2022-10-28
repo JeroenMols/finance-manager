35a2df334c0d05e2c7c088bce3d608da2899eb46
Jeroen Mols <jeroen@jeroenmols.com>
2022-10-28 20:32:40 +0200

    Reformat code

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import React, { CSSProperties } from 'react';
 import { useEffect, useState } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 import { useCookies } from 'react-cookie';
-import { log } from './log'
+import { log } from './log';
 
-const base_url = process.env.REACT_APP_SERVER_URL
+const base_url = process.env.REACT_APP_SERVER_URL;
 
 console.log(`
 
@@ -18,75 +18,91 @@ console.log(`
 Built by Jeroen Mols
 Checkout his portfolio at https://jeroenmols.com
 
-`                           
-)
+`);
 
-function csvToHoldings(csv: string) : Holding[] {
-  let allValues = csv.split(',')
-  let holdings : Holding[] = [];
+function csvToHoldings(csv: string): Holding[] {
+  let allValues = csv.split(',');
+  let holdings: Holding[] = [];
   for (let i = 0; i < allValues.length; i = i + 2) {
-    holdings.push({ ticker: allValues[i].trim(), shares: parseInt(allValues[i + 1]) });
+    holdings.push({
+      ticker: allValues[i].trim(),
+      shares: parseInt(allValues[i + 1]),
+    });
   }
-  return holdings
+  return holdings;
 }
 
 function holdingsToCsv(holdings: Holding[]) {
-  return holdings.map((a) => a.ticker + "," + a.shares).reduce((a, b) => (a + "," + b))
+  return holdings
+    .map((a) => a.ticker + ',' + a.shares)
+    .reduce((a, b) => a + ',' + b);
 }
 
-
-const defaultHoldings = "VOO,6,IWDA.AS,30,MSFT,3,AAPL,5,TSLA,4,GOOG,7,NVDA,6,AMZN,7"
+const defaultHoldings =
+  'VOO,6,IWDA.AS,30,MSFT,3,AAPL,5,TSLA,4,GOOG,7,NVDA,6,AMZN,7';
 
 type Holding = {
-  ticker: string,
-  shares: number,
-}
+  ticker: string;
+  shares: number;
+};
 
 const App = () => {
-
   const [cookies, setCookie] = useCookies(['stocks']);
 
-  const [ticker, setTicker] = useState("");
-  const [shares, setShares] = useState("");
-  const [toImport, setToImport] = useState("");
-  const [holdings, setHoldings] = useState<Holding[]>(csvToHoldings(cookies.stocks ? cookies.stocks : defaultHoldings));
-
+  const [ticker, setTicker] = useState('');
+  const [shares, setShares] = useState('');
+  const [toImport, setToImport] = useState('');
+  const [holdings, setHoldings] = useState<Holding[]>(
+    csvToHoldings(cookies.stocks ? cookies.stocks : defaultHoldings)
+  );
 
   const addHolding = (event: React.FormEvent) => {
     event.preventDefault();
-    let newHoldings = [...holdings, { ticker: ticker, shares: parseInt(shares) }]
+    let newHoldings = [
+      ...holdings,
+      { ticker: ticker, shares: parseInt(shares) },
+    ];
     setHoldings(newHoldings);
-    setTicker("");
-    setShares("");
-    setCookie('stocks', holdingsToCsv(newHoldings))
-  }
+    setTicker('');
+    setShares('');
+    setCookie('stocks', holdingsToCsv(newHoldings));
+  };
 
   const importHoldings = (event: React.FormEvent) => {
     event.preventDefault();
-    let imported = csvToHoldings(toImport)
+    let imported = csvToHoldings(toImport);
     setHoldings(imported);
-    setCookie('stocks', toImport)
-    setToImport("");
-  }
+    setCookie('stocks', toImport);
+    setToImport('');
+  };
 
-  const exportHoldings = () => { alert(holdingsToCsv(holdings)); }
-  log('Cookie: ' + cookies)
+  const exportHoldings = () => {
+    alert(holdingsToCsv(holdings));
+  };
+  log('Cookie: ' + cookies);
 
   return (
     <div className="App">
       <h1>Welcome to the stocks app</h1>
       <form onSubmit={addHolding}>
-        <label>Add holding:
+        <label>
+          Add holding:
           <input
             type="text"
-            placeholder='Ticker'
+            placeholder="Ticker"
             value={ticker}
-            onChange={e => { setTicker(e.target.value) }} />
+            onChange={(e) => {
+              setTicker(e.target.value);
+            }}
+          />
           <input
             type="number"
-            placeholder='Amount of shares'
+            placeholder="Amount of shares"
             value={shares}
-            onChange={e => { setShares(e.target.value) }} />
+            onChange={(e) => {
+              setShares(e.target.value);
+            }}
+          />
         </label>
         <input type="submit" value="Add" />
       </form>
@@ -96,67 +112,85 @@ const App = () => {
         <input
           type="text"
           value={toImport}
-          onChange={e => { setToImport(e.target.value) }} />
+          onChange={(e) => {
+            setToImport(e.target.value);
+          }}
+        />
         <input type="submit" value="Import Stocks" />
       </form>
       <button onClick={exportHoldings}>Export stocks</button>
     </div>
   );
-}
+};
 
 export default App;
 
-const StockList = (props: {holdings: Holding[]}) => {
-
-  const [stockData, setStockData] = useState<StockData[]>([])
+const StockList = (props: { holdings: Holding[] }) => {
+  const [stockData, setStockData] = useState<StockData[]>([]);
 
   useEffect(() => {
-    let newStocksPromises : Promise<StockData>[] = []
+    let newStocksPromises: Promise<StockData>[] = [];
     for (let i = 0; i < props.holdings.length; i++) {
-      log("loading stock: " + props.holdings[i].ticker)
-      newStocksPromises.push(loadHolding(props.holdings[i]))
+      log('loading stock: ' + props.holdings[i].ticker);
+      newStocksPromises.push(loadHolding(props.holdings[i]));
     }
-    Promise.all(newStocksPromises).then(stocks => {
-      log(newStocksPromises)
-      setStockData(stocks)
-    })
-  }, [props])
+    Promise.all(newStocksPromises).then((stocks) => {
+      log(newStocksPromises);
+      setStockData(stocks);
+    });
+  }, [props]);
 
   const loadHolding = async (holding: Holding) => {
-    let response = await fetch(base_url + 'stocks/' + holding.ticker + "/" + holding.shares)
-    log("loaded stock: " + holding.ticker)
-    return await response.json() as StockData
-  }
+    let response = await fetch(
+      base_url + 'stocks/' + holding.ticker + '/' + holding.shares
+    );
+    log('loaded stock: ' + holding.ticker);
+    return (await response.json()) as StockData;
+  };
 
-  let stockElements : JSX.Element[] = []
+  let stockElements: JSX.Element[] = [];
   stockData.forEach((stock, index) => {
-    stockElements.push(<Stock style={{ backgroundColor: toColor(index) }} key={stock.ticker} stockData={stock} />)
-  })
+    stockElements.push(
+      <Stock
+        style={{ backgroundColor: toColor(index) }}
+        key={stock.ticker}
+        stockData={stock}
+      />
+    );
+  });
 
-  let totalPortfolioValue = 0
+  let totalPortfolioValue = 0;
   for (let i = 0; i < stockData.length; i++) {
-    log("Adding " + stockData[i].ticker + "   " + stockData[i].price)
-    totalPortfolioValue += stockData[i].totalValue
+    log('Adding ' + stockData[i].ticker + '   ' + stockData[i].price);
+    totalPortfolioValue += stockData[i].totalValue;
   }
 
-  let chart = <div />
+  let chart = <div />;
   if (stockData.length > 0) {
-    let chartData = stockData.map((data, index) => { return { title: data.name, value: data.totalValue, color: toColor(index) } })
-    chart = <div className="Chart">
-      <PieChart
-        radius={49}
-        data={chartData}
-        animate={true}
-        segmentsShift={1}
-      />
-    </div>
+    let chartData = stockData.map((data, index) => {
+      return {
+        title: data.name,
+        value: data.totalValue,
+        color: toColor(index),
+      };
+    });
+    chart = (
+      <div className="Chart">
+        <PieChart
+          radius={49}
+          data={chartData}
+          animate={true}
+          segmentsShift={1}
+        />
+      </div>
+    );
   }
 
   return (
-    <ul className='StockList' >
+    <ul className="StockList">
       {chart}
       {stockElements}
-      <li className='Stock'>
+      <li className="Stock">
         <div>Total portfolio value</div>
         <div></div>
         <div className="Value" />
@@ -164,33 +198,37 @@ const StockList = (props: {holdings: Holding[]}) => {
         <div className="Value">{totalPortfolioValue.toFixed(2)}</div>
       </li>
     </ul>
-  )
-}
+  );
+};
 
 type StockData = {
-  name: string,
-  ticker: string,
-  price: number,
-  shares: number,
-  totalValue: number
-}
+  name: string;
+  ticker: string;
+  price: number;
+  shares: number;
+  totalValue: number;
+};
 
-const Stock = (props : {stockData: StockData, style: CSSProperties} | {ticker: string}) => {
-
-  if ("ticker" in props) {
-    return <li>Loading ticker {props.ticker} </li>
+const Stock = (
+  props: { stockData: StockData; style: CSSProperties } | { ticker: string }
+) => {
+  if ('ticker' in props) {
+    return <li>Loading ticker {props.ticker} </li>;
   } else {
-    return <li className="Stock" {...props}>
-      <div>{props.stockData.name}</div>
-      <div>{props.stockData.ticker}</div>
-      <div className="Value">{props.stockData.price.toFixed(2)}</div>
-      <div className="Value">{props.stockData.shares}</div>
-      <div className="Value">{props.stockData.totalValue.toFixed(2)}</div>
-    </li>
+    return (
+      <li className="Stock" {...props}>
+        <div>{props.stockData.name}</div>
+        <div>{props.stockData.ticker}</div>
+        <div className="Value">{props.stockData.price.toFixed(2)}</div>
+        <div className="Value">{props.stockData.shares}</div>
+        <div className="Value">{props.stockData.totalValue.toFixed(2)}</div>
+      </li>
+    );
   }
-}
+};
 
-const colors = ['#C62828',
+const colors = [
+  '#C62828',
   '#AD1457',
   '#6A1B9A',
   '#283593',
@@ -202,8 +240,9 @@ const colors = ['#C62828',
   '#EF6C00',
   '#D84315',
   '#4E342E',
-  '#37474F']
+  '#37474F',
+];
 
 function toColor(index: number) {
-  return colors[index % colors.length]
+  return colors[index % colors.length];
 }
