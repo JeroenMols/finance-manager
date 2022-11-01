bba1a47265500eaf2e8f927103dfe72288ce54cd
Jeroen Mols <jeroen@jeroenmols.com>
2022-11-01 15:48:27 +0100

    Refactor stocks to a separate screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+console.log(`
+
+          __                            
+         / /__  _________  ___  ____    
+    __  / / _ \\/ ___/ __ \\/ _ \\/ __ \\   
+   / /_/ /  __/ /  / /_/ /  __/ / / /  
+   \\____/\\___/_/   \\____/\\___/_/ /_/
+
+Built by Jeroen Mols
+Checkout his portfolio at https://jeroenmols.com
+
+`);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
