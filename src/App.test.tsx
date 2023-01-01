ee46644ab8b5f0c67788399e23333581cd9b9aa8
Jeroen Mols <jeroen@jeroenmols.com>
2023-01-01 21:19:08 +0100

    remove unused files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Stocks from './stocks';
-
-test('renders learn react link', () => {
-  // render(<Stocks />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
